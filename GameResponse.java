92091cdcaf3d4ec793f9bd0c46a98468ec551b53
Iliya Baranov <123483598+IliyaBaranov@users.noreply.github.com>
2025-03-20 13:59:28 +0200

    Add files via upload

---
+++ GameResponse.java
@@ -0,0 +1,21 @@
+package ee.mainor.classroom.greeting.dto;
+
+import java.util.UUID;
+
+public class GameResponse {
+    private UUID gameId;
+    private String message;
+
+    public GameResponse(UUID gameId, String message) {
+        this.gameId = gameId;
+        this.message = message;
+    }
+
+    public UUID getGameId() {
+        return gameId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
